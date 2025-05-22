20e98bb8b9b6191fc6ce33aa02e0f113561c795a
Seriously Blank <siriusblackrc@gmail.com>
2025-05-22 10:30:13 +0530

    file path not hardcoded

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import no_words, ch_dict
+import sys
 
 def get_book_text(path_to_file):
 	with open(path_to_file,encoding='utf-8') as f:
@@ -6,9 +7,14 @@ def get_book_text(path_to_file):
 		return file_contents
 
 def main():
-	content = get_book_text("books/frankenstein.txt")
+	if len(sys.argv) >= 2:
+		file_path = sys.argv[1]
+	else:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	content = get_book_text(file_path)
 	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
+	print(f"Analyzing book found at {file_path}...")
 	wc = no_words(content)
 	print("----------- Word Count ----------")
 	print(f"Found {wc} total words")
